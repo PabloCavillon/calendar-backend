d6b6e5e1e4c1889131c6607dff500714f96090df
Sparky <pabloa.cavillon@gmail.com>
2020-10-15 19:09:15 -0300

    v1 Calendar App

---
+++ public/precache-manifest.739afc084458f0ebcbba316ba98fbaf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1746ad8fbe148d8882958ee894a508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8612d4c2c47a03013250",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "41791ad9401b82053bdb",
+    "url": "/static/css/main.b4e44e7d.chunk.css"
+  },
+  {
+    "revision": "8612d4c2c47a03013250",
+    "url": "/static/js/2.c2bdd135.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.c2bdd135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41791ad9401b82053bdb",
+    "url": "/static/js/main.8ef4d07e.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
